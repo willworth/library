2be6291d96903c738c53108f746b2cd3344d80ba
Will Worth <will.worth@gmail.com>
2018-04-11 14:37:53 +0200

    bootstrap and jquery

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ const chalk = require('chalk');
 const debug = require('debug')('app');  // app will tell you where you are in the log!
 const morgan = require('morgan');
 const app = express();
+const path = require('path');  //built in!
 
 
 // app.use(morgan('combined'));
 app.use(morgan('tiny'));
 
 app.get('/', function(req, res){
-    res.send("Hi");
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 })
+
+
+// app.get('/', function(req, res){
+//     res.send("Hi");
+// })
 
 
 app.listen(3000, function(){
